d21dab22999d10124032c7162aaa4005de1fd9f0
Helena Paixão <hp.helenapaixao@gmail.com>
2018-11-10 17:01:32 -0300

    Create Timeline.js

---
+++ src/pages/Timeline.js
@@ -0,0 +1,53 @@
+
+import React, {Component} from 'react';
+import api from '../services/api';
+
+
+import twitterLogo from '../twitter.svg'
+
+import './Timeline.css';
+
+
+
+export default class Timeline extends Component{
+    state = {
+
+        newTweet: '',
+    };
+    handleNewTweet  = async e => {
+
+        if(e.keycode =! 13) return;
+
+        const content = this.state.newTweet;
+        const author = localStorage.getItem("@GoTwitter: username");
+
+              await api.post('tweets', {content, author})
+
+        this.setState({newTweet: ''});
+
+
+    };
+
+    handleInputChange = e =>{
+        this.setState({newTweet: e.target.value});
+    };
+
+    render(){
+        return(
+
+            <div className="timeline-wrapper">
+            <img height={24} src={twitterLogo} alt="GoTwitter" />
+
+            <form>
+                <textarea
+                value={this.state.newTweet}
+                onChange={this.handleInputChange}
+                onKeyDown={this.handleNewTweet}
+                placeholder="O que está acontecendo?"
+                />
+                </form>
+            </div>
+
+        );
+    }
+}
